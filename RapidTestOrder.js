0a07a5721a06801e30933c86acda001924b09482
coortoise <curtis2002@hotmail.ca>
2024-02-13 15:52:09 -0500

    complete

--- RapidTestOrder.js
+++ RapidTestOrder.js
@@ -5,27 +5,47 @@ class RapidTestOrder {
         let aReturn = [];
         this.stateCur = this.OrderState.SIZE;
         aReturn.push("Welcome to Curtis' Coffee Club.");
-        aReturn.push("What size coffee would you like?");
-        return aReturn;
-      },
-      SIZE: () => {
-        let aReturn = [];
-        this.stateCur = this.OrderState.RESERVING;
         aReturn.push("How would you like your coffee?");
         aReturn.push("Black or Regular?");
+        return aReturn;
+      },
+    SIZE: (sInput) => {
+        let aReturn = [];
+        this.stateCur = this.OrderState.SIDE;
+        if (sInput.toLowerCase().startsWith('b')) {
+          aReturn.push("Alright, so a black coffee.");
+          aReturn.push("What size? Small or Large?");
+        } else {
+          aReturn.push("Alright, so a regular.");
+          aReturn.push("What size? Small or Large?")
+        }
+        return aReturn;
+      },
+    SIDE: (sInput) => {
+        let aReturn = [];
+        this.stateCur = this.OrderState.RESERVING;
+        if (sInput.toLowerCase().startsWith('s')) {
+          aReturn.push("Small coffee.");
+          aReturn.push("Would you like a donut on the side?");
+        } else {
+          aReturn.push("Large coffee");
+          aReturn.push("Would you like a donut on the side?")
+        }
         return aReturn;
       },
       RESERVING: (sInput) => {
         let aReturn = [];
         this.isDone = true;
         if (sInput.toLowerCase().startsWith('y')) {
-          aReturn.push(`Your rapid test is reserved under the phone number ${this.sFrom}`);
+          aReturn.push("Alright so one coffee and one donut.");
           let d = new Date();
           d.setMinutes(d.getMinutes() + 120);
           aReturn.push(`Please pick it up at 123 Tidy St., Acton before ${d.toTimeString()}`);
         } else {
-          aReturn.push("Please leave if you are not here to order");
-          aReturn.push("Bye bye now")
+            let d = new Date();
+          d.setMinutes(d.getMinutes() + 120);
+          aReturn.push("Alright so just the one coffee.");
+          aReturn.push(`Please pick it up at 123 Tidy St., Acton before ${d.toTimeString()}`)
         }
         return aReturn;
       }
